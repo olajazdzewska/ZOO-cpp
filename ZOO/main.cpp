28f8369caabd9cf18bc85320fd55f5ee59a6e40d
olajazdzewska <129542964+olajazdzewska@users.noreply.github.com>
2023-04-06 17:19:05 +0200

    Initial Commit

---
+++ ZOO/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ZOO
+//
+//  Created by Aleksandra Jażdżewska on 06/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
